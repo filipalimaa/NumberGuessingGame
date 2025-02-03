b25447a1c52b1f23c1af7c1ebc604b2defdd7fd9
filipalimaa <filipalima1995@gmail.com>
2025-02-03 18:04:33 +0000

    Done for today

--- src/main.py
+++ src/main.py
@@ -24,10 +24,31 @@ def difficultyLevel():
         
         if difficulty == "1":
             print("Great! You have selected the Easy difficulty level.")
+            return 10
         elif difficulty == "2":
             print("Great! You have selected the Medium difficulty level.")
+            return 5
         elif difficulty == "3":
             print("Great! You have selected the Hard difficulty level.")
+            return 3
         else:
             print("Invalid option. Please, select between 1 (Easy), 2 (Medium) or 3 (Hard).")            
           
+### Rest of the requirements: Definition of the rest of the game
+
+def game():
+    welcomeMessage()
+    secretNo = rd.randint(1, 100)
+    attempts = difficultyLevel()
+    usedAttempts = 0
+    
+    while usedAttempts > 0:
+        try:
+            guess = int(input(f"Enter your guess: "))
+        except ValueError:
+            print("Please insert a valid number.")
+            continue
+        
+        usedAttempts += 1
+        attempts -= 1
+        
