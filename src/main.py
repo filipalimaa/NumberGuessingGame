b4c364f6bb45bf65e207906d50e80c92675118d7
filipalimaa <filipalima1995@gmail.com>
2025-02-03 17:38:53 +0000

    Definição da mensagem inicial e regras do jogo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
-def main():
-    print("Olá, mundo! 🚀")
+import random as rd
+
+### First Requirement: Display a welcome message along with the rules when the game starts.
+
+def welcomeMessage():
+    print("""
+          ==========================================
+             Welcome to the Number Guessing Game!
+          ==========================================
+          I'm thinking of a number between 1 and 100.
+        You have 5 chances to guess the correct number.
+        
+        Please select the difficulty level:
+    1. Easy (10 chances)
+    2. Medium (5 chances)
+    3. Hard (3 chances)
     
-if __name__ == "__main__":
-    main()
+             GOOD LUCK!""")
